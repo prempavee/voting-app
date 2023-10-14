6cfd9e3dab5af70aee2a5fd2f8a158c61a3766fa
Tatiana <tarzhakova@gmail.com>
2023-10-14 12:31:43 +0200

    delete old

--- README.md
+++
@@ -1,9 +0,0 @@
-# Prempavee 1st GANJA CUP voting app
-
-
-## Admin only:
-`/admin`
-`/results`
-
-
-Build on Next.js
--- src/api/index.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const $host = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API
-})
-
-export {
-  $host
-}

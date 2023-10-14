f774ef63f93f86c6f49bffe5d51ec290f8ba87b9
Tatiana <tarzhakova@gmail.com>
2023-10-14 12:49:23 +0200

    clear

--- src/pages/results.js
+++ src/pages/results.js
@@ -1,20 +1,14 @@
 import { useState } from 'react'
 import MainContainer from '@/components/MainContainer'
-import { useSnackbar } from 'notistack'
 import Loading from '@/components/Loading'
 import ResultsRounds from '@/components/RoundsListForResults'
 import RoundResults from '@/components/RoundResults'
 import CurrentRoundInfo from '@/components/CurrentRoundInfo'
-// import SamplesInfo from '@/components/SamplesInfo'
 import { ROUNDS } from '@/apollo/roundsQueries'
 import { useQuery } from '@apollo/client'
 
 const Results = () => {
-  const { enqueueSnackbar } = useSnackbar()
-  // const [loading, setLoading] = useState(true)
-
   const { loading, error: roundsError, data: roundsData } = useQuery(ROUNDS)
-
   const [choosenRound, setChoosenRound] = useState(null)
 
   const handleClickRound = async (id) => {
@@ -25,7 +19,6 @@ const Results = () => {
 
   return (
     <MainContainer title='Results'>
-      {/* <SamplesInfo /> */}
       <ResultsRounds rounds={roundsData.rounds} handleClickRound={handleClickRound} />
       <CurrentRoundInfo />
       <RoundResults round={choosenRound} rounds={roundsData.rounds}/>
